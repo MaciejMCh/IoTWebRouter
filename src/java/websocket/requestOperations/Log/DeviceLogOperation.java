b62cc6c68899ead70471d6703936b445afdb4cd1
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2015-10-31 21:49:43 +0100

    reflection works

--- src/java/websocket/requestOperations/Log/DeviceLogOperation.java
+++ src/java/websocket/requestOperations/Log/DeviceLogOperation.java
@@ -5,9 +5,14 @@
  */
 package websocket.requestOperations.Log;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.Session;
+import model.Interactor;
+import model.LogParser;
 import org.json.JSONObject;
 
 /**
@@ -20,10 +25,22 @@ public class DeviceLogOperation extends InterpretedLogOperation {
     
     public DeviceLogOperation(JSONObject params, Session session) {
         super(params, session);
-        String sisi = "asdas";
-        String sisis = "asdas";
-//        set(this, "deviceID", "dupaa");
     }
+
+    @Override
+    public void performOperation() {
+        if (this.deviceID != null) {
+            
+        } else {
+            try {
+                this.session.getBasicRemote().sendText(LogParser.parseDevices(Interactor.getInstance().enviroment.devices));
+            } catch (IOException ex) {
+                Logger.getLogger(DeviceLogOperation.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
 
     @Override
     public ArrayList<String> propertyNames() {
--- src/java/websocket/requestOperations/Log/InterpretedLogOperation.java
+++ src/java/websocket/requestOperations/Log/InterpretedLogOperation.java
@@ -7,7 +7,9 @@ package websocket.requestOperations.Log;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Collection;
 import javax.websocket.Session;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import websocket.requestOperations.RequestOperation;
 
@@ -26,6 +28,13 @@ public class InterpretedLogOperation extends RequestOperation {
         list = this.propertyNames();
         list = this.propertyNames();
         
+//        this.arguments = params.getJSONArray("arguments");
+        JSONArray argumentsJSONArray = params.getJSONArray("arguments");
+        this.arguments = new ArrayList();
+        for (int i=0; i<= argumentsJSONArray.length() - 1; i++) {
+            this.arguments.add(argumentsJSONArray.getString(i));
+        }
+        
         this.mapArgumentsToProperties();
 //        this.arguments = params.getJSONArray("arguments").;
 //        this.options = patams.get
@@ -33,10 +42,14 @@ public class InterpretedLogOperation extends RequestOperation {
     
     public void mapArgumentsToProperties() {
 //        this.propertyNames()[0]
-          reftSet(this, this.propertyNames().get(0), "duu");
-//        for (String propertyName : this.propertyNames()) {
-//            set(this, propertyName, "duuuuu");
-//        }
+//          reftSet(this, this.propertyNames().get(0), "duu");
+        int i = 0;
+        for (String propertyName : this.propertyNames()) {
+            if (this.arguments.size() > i) {
+                reftSet(this, propertyName, this.arguments.get(i));
+            }
+            i++;
+        }
     }
     
     public ArrayList<String> propertyNames() {
