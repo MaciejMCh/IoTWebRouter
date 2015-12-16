dd2c80889ce3e3ad36d7b021a2df2824bc6dee29
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2015-12-16 18:00:13 +0100

    device controller uses play medium

--- app/controllers/DeviceController.java
+++ app/controllers/DeviceController.java
@@ -3,6 +3,11 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.WebSocket;
+import play.libs.F.Callback;
+import com.google.gson.*;
+import websocket.requestOperations.RequestOperation;
+import websocket.requestOperations.RequestOperationsSerializer;
+
 // import model.Device;
 
 public class DeviceController extends Controller {
@@ -10,8 +15,15 @@ public class DeviceController extends Controller {
     public static WebSocket<String> device() {
         return new WebSocket<String>() {
             public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {
-                out.write("device connected");
-            }
+                in.onMessage(new Callback<String>() {
+                    public void invoke(String event) {
+                        JsonObject json = new JsonParser().parse(event).getAsJsonObject();
+                        RequestOperation operation;
+                        operation = RequestOperationsSerializer.serializeOperation(json, new PlayWebSocketMedium(out));
+                        operation.performOperation();
+                    }
+                }
+            );}
         };
     };
     
---
+++ app/controllers/PlayWebSocketMedium.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import model.Medium;
+import play.mvc.WebSocket;
+
+class PlayWebSocketMedium implements Medium {
+    
+    protected WebSocket.Out<String> webSocketOut;
+    
+    public PlayWebSocketMedium(WebSocket.Out<String> webSocketOut) {
+        super();
+        this.webSocketOut = webSocketOut;
+    }
+    
+    @Override
+    public void sendMessage(String message) {
+        
+    }
+    
+}
