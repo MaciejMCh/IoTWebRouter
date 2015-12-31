937f8758221dda7e49d34acb24d70cd83a7c6376
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2015-12-31 17:56:59 +0100

    error model test

---
+++ test/requestOperations/ErrorTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package requestOperations;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author maciej
+ */
+public class ErrorTest {
+    
+    public ErrorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getErrorMessage method, of class Error.
+     */
+    @Test
+    public void testInitialization() {
+        Error error = new Error("error");
+        assertEquals(error.getErrorMessage(), "error");
+    }
+    
+}
