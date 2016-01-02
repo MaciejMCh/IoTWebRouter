a8900d006d58fd75364ae9dc41905498866f2eb9
Maciej Chmielewski <maciej.chmielewski@appunite.com>
2016-01-02 16:18:33 +0100

    device reconnect notification test

--- test/notificationCenter/DeviceReconnectNotificationTest.java
+++ test/notificationCenter/DeviceReconnectNotificationTest.java
@@ -5,6 +5,13 @@
  */
 package notificationCenter;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Device;
+import model.ModelSerializer;
+import model.SerializationErrorException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -41,14 +48,17 @@ public class DeviceReconnectNotificationTest {
      * Test of notificationType method, of class DeviceReconnectNotification.
      */
     @Test
-    public void testNotificationType() {
-        System.out.println("notificationType");
-        DeviceReconnectNotification instance = null;
-        String expResult = "";
-        String result = instance.notificationType();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testInitialization() {
+        try {
+            JsonObject json = new JsonParser().parse("{\"name\":\"sensor\",\"interfaces\":[{\"direction\":\"output\",\"data_type\":\"light\",\"id\":\"int_li_in\"}]}").getAsJsonObject();
+            Device device = (Device) ModelSerializer.model(Device.class, json);
+            DeviceReconnectNotification instance = new DeviceReconnectNotification(device);
+            JsonObject expResult = new JsonParser().parse("{\"notification_type\":\"device_reconnect\",\"subject\":{\"id\":\"" + device.getId() + "\",\"name\":\"sensor\",\"interfaces\":[{\"id\":\"int_li_in\",\"data_type\":\"light\",\"direction\":\"output\"}]}}").getAsJsonObject();
+            JsonObject result = instance.jsonRepresentation();
+            assertEquals(expResult, result);
+        } catch (SerializationErrorException ex) {
+            fail(ex.toString());
+        }
     }
     
 }
